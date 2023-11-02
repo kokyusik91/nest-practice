1a56920cdf8f4453bcdd66e52e4da09cc71211b9
kyusikko <kokyusik91@naver.com>
2023-11-02 23:20:00 +0900

    ✨ post controller => service로 비즈니스 로직을 옮겼다

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,48 +1,13 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 10000000,
-    commentCount: 999999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '노래 연습하고 있는 해린',
-    likeCount: 10000000,
-    commentCount: 9999299,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랙핑크 로제',
-    content: '종운에서 노래하고 있는 로제',
-    likeCount: 10000000,
-    commentCount: 9999299,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -50,20 +15,13 @@ export class PostsController {
 
   @Get()
   getPosts() {
-    console.log(posts);
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // path parameter
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   @Post()
@@ -72,17 +30,21 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
+    return this.postsService.createPost(author, title, content);
+  }
 
-    posts = [...posts, post];
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    return this.postsService.updatePost(+id, author, title, content);
+  }
 
-    return post;
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(+id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PostsModel } from './entities/posts.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(PostsModel)
+    private readonly postsRepository: Repository<PostsModel>,
+  ) {}
+
+  async getAllPosts() {
+    return this.postsRepository.find();
+  }
+
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  async createPost(author: string, title: string, content: string) {
+    // 1) create => 저장할 객체를 생성한다.
+    // 2) save => 객체를 생성한다. (create 메서드에서 생성한 객체로)
+
+    const post = this.postsRepository.create({
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    });
+
+    // id가 추가된 post
+    const newPost = await this.postsRepository.save(post);
+
+    return newPost;
+  }
+
+  async updatePost(
+    postId: number,
+    author: string,
+    title: string,
+    content: string,
+  ) {
+    // save 기능
+    // 1) 만약에 데이터가 존재하지 않는다면 (id 기준으로) 새로 생성한다.
+    // 2) 만약에 데이터가 존재한다면 (같은 id의 값이 존재한다면) 존재하던 값을 업데이트한다.
+
+    const post = await this.postsRepository.findOne({
+      where: {
+        id: postId,
+      },
+    });
+    // post 업스면
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    // post는 id가 존재하는 post이다.
+    const newPost = await this.postsRepository.save(post);
+
+    return newPost;
+  }
+
+  async deletePost(id: number) {
+    const post = await this.postsRepository.findOne({ where: { id } });
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    await this.postsRepository.delete(id);
+
+    return id;
+  }
+}
