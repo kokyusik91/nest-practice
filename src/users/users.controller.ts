203f554fd2ae096af320b2d9cb14cda8e61e125e
kyusikko <kokyusik91@naver.com>
2023-12-17 12:25:52 +0900

    ✨ user관련 class validator 과 class transfomer를 사용

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  postUser(
-    @Body('nickname') nickname: string,
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return this.usersService.createUser(email, nickname, password);
-  }
+  // auth에서 관리하므로 삭제
+  // @Post()
+  // postUser(
+  //   @Body('nickname') nickname: string,
+  //   @Body('email') email: string,
+  //   @Body('password') password: string,
+  // ) {
+  //   return this.usersService.createUser({ email, nickname, password });
+  // }
 
   @Get()
+  // @UseInterceptors(ClassSerializerInterceptor)
+  /**
+   * serialization -> 직렬화 -> 현재 시스템에서 사용되는 (NestJS) 데이터의 구조를 다른 시스템에서도 쉽게 사용 할 수 있는 포맷으로 변환
+   *
+   * -> class의 Object에서 JSON 포맷으로 변환
+   */
   getUser() {
     return this.usersService.getAllUser();
   }
