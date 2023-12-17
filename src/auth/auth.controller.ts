921bbf0ebd8b6d82c9d4550b9279526e20fb7c25
kyusikko <kokyusik91@naver.com>
2023-12-17 12:23:47 +0900

    ✨ 가드 사용하기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import {
-  MaxLengthPipe,
-  MinLengthPipe,
-  // PasswordPipe,
-} from './pipe/password.pipe';
+// import {
+//   MaxLengthPipe,
+//   MinLengthPipe,
+//   // PasswordPipe,
+// } from './pipe/password.pipe';
 import { BasicTokenGuard } from './guard/basic-token.guard';
-import { RefreshTokenGuard } from './guard/bearer-token.guard';
+import { AccessTokenGuard } from './guard/bearer-token.guard';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -14,7 +15,7 @@ export class AuthController {
 
   // AccessToken 새로 발급 받는 api
   @Post('token/access')
-  @UseGuards(RefreshTokenGuard)
+  @UseGuards(AccessTokenGuard)
   postTokenAccess(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, true);
 
@@ -30,7 +31,7 @@ export class AuthController {
 
   // RefreshToken 새로 발급 받는 api
   @Post('token/refresh')
-  @UseGuards(RefreshTokenGuard)
+  @UseGuards(AccessTokenGuard)
   postRefreshAccess(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, true);
 
@@ -54,15 +55,21 @@ export class AuthController {
     return this.authService.loginWithEmail(credentials);
   }
 
-  // 회원 가입
+  // 회원 가입 => RegisterUserDto + Class validator 사용
   @Post('register/email')
-  postRegisterEmail(
-    @Body('nickname') nickname: string,
-    @Body('email') email: string,
-    // 인스턴스화 해서 넣을때 장점 : constructor에 인자를 넘길 수 있음.
-    @Body('password', new MaxLengthPipe(8), new MinLengthPipe(3))
-    password: string,
-  ) {
-    return this.authService.registerWithEmail({ nickname, password, email });
+  postRegisterEmail(@Body() body: RegisterUserDto) {
+    return this.authService.registerWithEmail(body);
   }
+
+  // 기존 회원가입 Controller logic => Pipe 사용
+  // @Post('register/email')
+  // postRegisterEmail(
+  //   @Body('nickname') nickname: string,
+  //   @Body('email') email: string,
+  //   // 인스턴스화 해서 넣을때 장점 : constructor에 인자를 넘길 수 있음.
+  //   @Body('password', new MaxLengthPipe(8), new MinLengthPipe(3))
+  //   password: string,
+  // ) {
+  //   return this.authService.registerWithEmail({ nickname, password, email });
+  // }
 }
