e930f1d3c8eb1c4a2d9bc4edee8d4aa932b876cd
kyusikko <kokyusik91@naver.com>
2023-12-18 22:37:13 +0900

    ✨ dto에 해당하지 않는 쿼리들이 들어왔을때 에러 처리하는 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true,
       },
+      whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
   await app.listen(3000);
