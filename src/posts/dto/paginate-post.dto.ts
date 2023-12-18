e2880775d2d466c6877e285180df8c77aae97f4f
kyusikko <kokyusik91@naver.com>
2023-12-18 22:35:23 +0900

    ✨ 페이지 네이션 일반화 함수 DTO

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -1,30 +1,14 @@
-import { IsIn, IsNumber, IsOptional } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
+import { BasePaginationDto } from 'src/common/dto/base-pagination.dto';
 
-export class PaginatePostDto {
+export class PaginatePostDto extends BasePaginationDto {
+  // 좋아요 갯수 몇개 이상
   @IsNumber()
   @IsOptional()
-  page?: number;
+  where__likeCount__more_than: number;
 
+  // 제목에 무엇이 ''이 포함된 것들
   @IsNumber()
   @IsOptional()
-  where__id_less_than?: number;
-  // 이전 마지막 데이터의 ID
-  // 이 프로퍼티에 입력된 ID 보다 높은 ID 부터 값을 가져오기
-  // @Type(() => Number)
-  @IsNumber()
-  @IsOptional()
-  where__id_more_than?: number;
-
-  // 정렬
-  // createdAt -> 생성된 시간의 내림차/오름차 순으로 정렬
-  // list에 있는 값들만 허용이 된다.
-  // 예시) 'ASC'와 'DESC'만 들어와야 validation pass가 된다.
-  @IsIn(['ASC', 'DESC'])
-  @IsOptional()
-  // eslint-disable-next-line @typescript-eslint/prefer-as-const
-  order__createdAt: 'ASC' | 'DESC' = 'ASC';
-
-  @IsNumber()
-  @IsOptional()
-  take: number = 20;
+  where__title_i_like: string;
 }
