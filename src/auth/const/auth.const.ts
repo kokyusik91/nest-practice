f5f3125a870dd00365736ecc1efb4e21128ac44d
kyusikko <kokyusik91@naver.com>
2023-12-20 20:47:40 +0900

    ✨ env파일에 매핑되는 KEY 변수들을 생성

--- src/auth/const/auth.const.ts
+++
@@ -1,2 +0,0 @@
-export const JWT_SECRET = 'jamsilcrop';
-export const HASH_ROUNDS = 10;
---
+++ src/common/const/env-keys.const.ts
@@ -0,0 +1,21 @@
+// 서버 프로포토콜 => http / https
+export const ENV_PROTOCOL_KEY = 'PROTOCOL';
+
+// 서버 호스트 -> localhost:3000
+export const ENV_HOST_KEY = 'HOST';
+
+// JWT 토큰 시크릣 -> Jamsilcrop
+export const ENV_JWT_SECRET_KEY = 'JWT_SECRET';
+
+// JWT 토큰 해시 라운드 수 -> 10
+export const ENV_HASH_ROUNDS_KEY = 'HASH_ROUNDS';
+
+export const ENV_DB_HOST_KEY = 'DB_HOST';
+
+export const ENV_DB_PORT_KEY = 'DB_PORT';
+
+export const ENV_DB_USERNAME_KEY = 'DB_USERNAME';
+
+export const ENV_DB_PASSWORD_KEY = 'DB_PASSWORD';
+
+export const ENV_DB_DATABASE_KEY = 'DB_DATABASE';
