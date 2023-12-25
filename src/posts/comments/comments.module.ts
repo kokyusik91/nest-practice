70dad77bd2e333a259c600c2bf994293bb633346
kyusikko <kokyusik91@naver.com>
2023-12-25 11:46:57 +0900

    ✨ CommentsModule에서 사용되는 외부 Module들 imports

--- src/posts/comments/comments.module.ts
+++ src/posts/comments/comments.module.ts
@@ -3,10 +3,18 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentsModel } from './entity/comments.entity';
+import { CommonModule } from 'src/common/common.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   // CommentModel과 관련된 레포지토리 사용가능
-  imports: [TypeOrmModule.forFeature([CommentsModel])],
+  imports: [
+    TypeOrmModule.forFeature([CommentsModel]),
+    CommonModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
