3b8ccfd1fdd0daaf559edf80db7d1558967041e8
kyusikko <kokyusik91@naver.com>
2023-12-25 11:46:35 +0900

    ✨  댓글 모두 조회, 특정 댓글 조회, 댓글 생성 API 만들기

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { PaginateCommentsDto } from './dto/paginate-comments.dto';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+import { CreateCommentsDto } from './dto/create-comments.dto';
+import { User } from 'src/users/decorator/user.decorator';
+import { UsersModel } from 'src/users/entities/users.entity';
 
 @Controller('posts/:postId/comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @Get()
+  getComments(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Query() query: PaginateCommentsDto,
+  ) {
+    return this.commentsService.paginateComments(query, postId);
+  }
+
+  @Get(':commentId')
+  getComment(@Param('commentId', ParseIntPipe) commentId: number) {
+    console.log('commentId', commentId);
+    return this.commentsService.getCommentById(commentId);
+  }
+
+  @Post()
+  @UseGuards(AccessTokenGuard)
+  postComment(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Body() body: CreateCommentsDto,
+    @User() user: UsersModel,
+  ) {
+    return this.commentsService.createComment(body, postId, user);
+  }
 }
--- src/posts/comments/comments.service.ts
+++ src/posts/comments/comments.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PaginateCommentsDto } from './dto/paginate-comments.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentsModel } from './entity/comments.entity';
+import { Repository } from 'typeorm';
+import { CommonService } from 'src/common/common.service';
+import { CreateCommentsDto } from './dto/create-comments.dto';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { DEFAULT_COMMENT_FIND_OPTIONS } from './const/default-comment-find-options.const';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(CommentsModel)
+    private readonly commentsRepository: Repository<CommentsModel>,
+    private readonly commonService: CommonService,
+  ) {}
+
+  paginateComments(dto: PaginateCommentsDto, postId: number) {
+    return this.commonService.paginate(
+      dto,
+      this.commentsRepository,
+      {
+        where: {
+          post: {
+            id: postId,
+          },
+        },
+        ...DEFAULT_COMMENT_FIND_OPTIONS,
+      },
+      `posts/${postId}/comments`,
+    );
+  }
+
+  async getCommentById(id: number) {
+    const comment = await this.commentsRepository.findOne({
+      where: {
+        id,
+      },
+      ...DEFAULT_COMMENT_FIND_OPTIONS,
+    });
+
+    if (!comment) {
+      throw new BadRequestException(`id : ${id} Comment는 존재하지 않습니다.`);
+    }
+
+    return comment;
+  }
+
+  async createComment(
+    dto: CreateCommentsDto,
+    postId: number,
+    author: UsersModel,
+  ) {
+    return this.commentsRepository.save({
+      ...dto,
+      // 어떤 post랑 연동시킬지?, id 기반으로 어떤 postId와 연결시킬지 지정을 해줌
+      post: {
+        id: postId,
+      },
+      // 유저는 그대로 넣어준다.
+      author,
+    });
+  }
+}
